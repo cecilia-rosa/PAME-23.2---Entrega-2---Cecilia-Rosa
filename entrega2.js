0ba05cb1c29e662c0b469cd7dfcb34d41952c299
Cecília Rosa <cecilianrosa.20231@poli.ufrj.br>
2024-01-17 01:38:21 -0300

    atualizacoes

--- entrega2.js
+++ entrega2.js
@@ -1,1 +1,23 @@
-console.log('oi')
+//marca que o remember me foi acionado
+
+function botaoRememberMe (){
+    var remember = true
+    return remember
+}
+
+//alerta com os dados apos botao sign in ser clicado
+function botaoSignIn(){
+    var nomeEmailInput = document.getElementById("nome_email");
+    var senhaInput = document.getElementById("senha");
+    var nomeEmailValue = nomeEmailInput.value;
+    var senhaValue = senhaInput.value;
+    var lembrar = remember
+    if (lembrar==true){
+        return alert("Username or Email: " + nomeEmailValue + "\nPassword: " + senhaValue + "\nLembrar:sim");
+    }
+    else if (lembrar!=true){
+        return alert("Username or Email: " + nomeEmailValue + "\nPassword: " + senhaValue + "\nLembrar:nao");
+    }
+}
+
+
